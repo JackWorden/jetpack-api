942e5f0cab17853957dcffdaa0646537cb77b0cf
Greg White <gkwhite95@gmail.com>
2016-04-05 19:31:28 -0400

    Adds Sprint model

---
+++ app/models/sprint.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: sprints
+#
+#  id         :integer          not null, primary key
+#  end_date   :date
+#  project_id :integer          not null
+#
+
+class Sprint < ActiveRecord::Base
+  belongs_to :project
+end
