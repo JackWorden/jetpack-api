ecc0a23436c803f74697525c18da78f18e9bdc01
JackWorden <JackWorden@knights.ucf.edu>
2016-04-07 00:31:01 -0400

    Adds Github info to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@
 #
 # Table name: public.users
 #
-#  id         :integer          not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  team_id    :integer
-#  uid        :integer
-#  name       :string
+#  id                  :integer          not null, primary key
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  team_id             :integer
+#  uid                 :integer
+#  name                :string
+#  github_access_token :string
+#  github_user_id      :integer
 #
 
 class User < ActiveRecord::Base
