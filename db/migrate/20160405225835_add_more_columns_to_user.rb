2bce89bca324342eb3dd7d800f56adfee5bf01d5
JackWorden <JackWorden@knights.ucf.edu>
2016-04-06 23:34:49 -0400

    Fixes team_id reference in User migration.

--- db/migrate/20160405225835_add_more_columns_to_user.rb
+++ db/migrate/20160405225835_add_more_columns_to_user.rb
@@ -1,6 +1,6 @@
 class AddMoreColumnsToUser < ActiveRecord::Migration
   def change
-    add_reference :users, :team_id, index: true
+    add_reference :users, :team, index: true
     add_column :users, :uid, :integer
     add_column :users, :name, :string
   end
