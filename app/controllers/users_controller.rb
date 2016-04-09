d2653638392793763387814e6808ef0a0310f824
JackWorden <JackWorden@knights.ucf.edu>
2016-04-08 23:01:49 -0400

    Refactors UsersController into UserFinder PORO.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def from_token
-    if params[:token]
-      render json: user_from_id, status: 200
-    else
-      render json: { errors: 'No access token given' }, status: 400
-    end
+    api_response = API::UserFinder.new(params[:token]).call
+    render json: api_response.data, status: api_response.status
   end
 
   def show
@@ -14,20 +11,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-  def user_from_id
-    user = User.find_by_github_id(github_id)
-    return user if user
-    User.create(github_id: github_id, name: octokit.user[:login])
-  end
-
-  def github_id
-    octokit.user[:id]
-  end
-
-  def octokit
-    @octokit ||= Octokit::Client.new(access_token: params[:token])
-  end
 
   def set_user
     @user = User.find(params[:id])
