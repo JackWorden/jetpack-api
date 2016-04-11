3b656ae880693cb804be5f49a03f2e8244ac0759
Greg White <gkwhite95@gmail.com>
2016-04-11 18:02:25 -0400

    Removes barewords %i() in ProjectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: %i(show update destroy)
+  before_action :set_project, only: [:show, :update, :destroy]
 
   def index
     render json: Project.all, status: :ok
