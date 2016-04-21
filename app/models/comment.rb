e72286f819dd33a1dcec4b56cdc2094359059e3e
Greg White <gkwhite95@gmail.com>
2016-04-20 22:43:28 -0400

    Adds Comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+  belongs_to :issue
+  belongs_to :user
+
+  validates :user, presence: true
+  validates :issue, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421002236) do
+ActiveRecord::Schema.define(version: 20160421023944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body",       null: false
+    t.integer  "issue_id",   null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "issues", force: :cascade do |t|
     t.integer "project_id",                   null: false
