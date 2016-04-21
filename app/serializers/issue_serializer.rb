81ac0baeb7587e3838048551135bc04fb8e487b7
Steven Petryk <me@stevenpetryk.com>
2016-04-21 19:21:28 -0400

    Annotate and add status to issue serializer

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,5 +1,5 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id, :description, :project_id, :sprint_id, :story_id, :assignee_id
+  attributes :id, :description, :project_id, :status, :sprint_id, :story_id, :assignee_id
   has_many :comments
   belongs_to :assignee
 end
