387e48440b58e0f3fc5e0b2f8b21b30c10ef0e0b
JackWorden <JackWorden@knights.ucf.edu>
2016-04-22 19:19:23 -0400

    Appeases Rubocop.

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,6 +1,7 @@
 class IssueSerializer < ApplicationSerializer
-  attributes :id, :description, :project_id, :status, :sprint_id,
-             :story_id, :assignee_id, :points, :order
+  attributes :id, :description, :project_id, :status, :order,
+             :sprint_id, :story_id, :assignee_id, :points
+
   has_many :comments
   belongs_to :assignee
 end
